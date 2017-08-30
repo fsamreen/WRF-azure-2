cd14ac5555702dd3d42d8f363dac3d60d5583eed
fsamreen <aziaf2002@yahoo.com>
2017-08-30 13:02:17 +0100

    Update install14.sh

--- install14.sh
+++ install14.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd ~/Build_WRF
 wget http://www2.mmm.ucar.edu/wrf/src/WRFV3.9.1.1.TAR.gz .
-gunzip WRFV3.8.1.TAR.gz
-tar -xf WRFV3.8.1.TAR
+gunzip WRFV3.9.1.1.TAR.gz
+tar -xf WRFV3.9.1.1.TAR
 cd WRFV3
 ./configure
+sudo apt-get install perl
+sudo apt-get install m4
