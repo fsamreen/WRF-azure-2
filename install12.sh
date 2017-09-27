9c68e5768fd9fb267e98b9cae82fc6e6084311eb
fsamreen <aziaf2002@yahoo.com>
2017-09-27 11:42:05 +0100

    Delete install12.sh

--- install12.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/Build_WRF/LIBRARIES
-tar xzvf jasper-1.900.1.tar.gz     #or just .tar if no .gz present
-cd jasper-1.900.1
-./configure --prefix=$DIR/grib2
-make
-make install
-cd ..
