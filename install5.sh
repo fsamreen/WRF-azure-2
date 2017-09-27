dc4be2988bd17d294c6fa91d14fb25114fb95ac5
fsamreen <aziaf2002@yahoo.com>
2017-09-27 11:42:57 +0100

    Delete install5.sh

--- install5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/Build_WRF/LIBRARIES
-tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
-cd netcdf-4.1.3
-./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
-sudo apt-get install make -y
-make
-make install
-cd ..
