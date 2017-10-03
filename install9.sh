9d51174b425ae734bc4ef87f92a71b8362e832a7
fsamreen <aziaf2002@yahoo.com>
2017-10-03 13:23:07 +0100

    Update install9.sh

--- install9.sh
+++ install9.sh
@@ -2,11 +2,11 @@
 # run as source xyz.sh as this is to set as enviroenmental variables.
 echo " Setting up environment variables for Zlib and WPS ..grib2......"
 cd ~/Build_WRF/LIBRARIES
-echo 'export LDFLAGS=-L$DIR/grib2/lib' >>~/.profile
-echo 'export CPPFLAGS=-I$DIR/grib2/include' >>~/.profile
-echo 'export JASPERLIB=$DIR/grib2/lib' >>~/.profile
-echo 'export JASPERINC=$DIR/grib2/include' >>~/.profile
-source ~/.profile
+#echo 'export LDFLAGS=-L$DIR/grib2/lib' >>~/.profile
+#echo 'export CPPFLAGS=-I$DIR/grib2/include' >>~/.profile
+#echo 'export JASPERLIB=$DIR/grib2/lib' >>~/.profile
+#echo 'export JASPERINC=$DIR/grib2/include' >>~/.profile
+#source ~/.profile
 #setenv JASPERLIB $DIR/grib2/lib
 #setenv JASPERINC $DIR/grib2/include
 #export LDFLAGS=-L$DIR/grib2/lib 
