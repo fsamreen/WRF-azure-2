9e52ba381d4ae58d77821f48b402a7bdb8f94488
fsamreen <aziaf2002@yahoo.com>
2017-10-03 13:28:18 +0100

    Update install4.sh

--- install4.sh
+++ install4.sh
@@ -8,7 +8,14 @@ echo 'export FC=gfortran' >>~/.profile
 echo 'export FCFLAGS=-m64' >>~/.profile
 echo 'export F77=gfortran' >>~/.profile
 echo 'export FFLAGS=-m64' >>~/.profile
-source ~/.profile
+echo 'export PATH=$DIR/netcdf/bin:$PATH' >>~/.profile
+echo 'export NETCDF=$DIR/netcdf' >>~/.profile
+echo 'export PATH=$DIR/mpich/bin:$PATH' >>~/.profile
+echo 'export LDFLAGS=-L$DIR/grib2/lib' >>~/.profile
+echo 'export CPPFLAGS=-I$DIR/grib2/include' >>~/.profile
+echo 'export JASPERLIB=$DIR/grib2/lib' >>~/.profile
+echo 'export JASPERINC=$DIR/grib2/include' >>~/.profile
+#source ~/.profile
 #export DIR=~/Build_WRF/LIBRARIES
 #export CC=gcc
 #export CXX=g++
@@ -16,6 +23,6 @@ source ~/.profile
 #export FCFLAGS=-m64
 #export F77=gfortran
 #export FFLAGS=-m64
-cd ~/WRF-azure
-echo "Calling script5....."
-sh ./install5.sh
+#cd ~/WRF-azure
+#echo "Calling script5....."
+#sh ./install5.sh
