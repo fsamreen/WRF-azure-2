5dab71a086d198cd21e32879af62513c35de96ef
fsamreen <aziaf2002@yahoo.com>
2017-10-03 13:22:28 +0100

    Update install8.sh

--- install8.sh
+++ install8.sh
@@ -2,8 +2,8 @@
 # run this as source xyz.sh
 echo " Setting up environment variable for mpich......"
 cd ~/Build_WRF/LIBRARIES
-echo 'export PATH=$DIR/mpich/bin:$PATH' >>~/.profile
-source ~/.profile
+#echo 'export PATH=$DIR/mpich/bin:$PATH' >>~/.profile
+#source ~/.profile
 #export PATH=$DIR/mpich/bin:$PATH
 cd ~/WRF-azure
 echo "Calling script9....."
