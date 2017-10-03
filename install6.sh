846c97fee5c51474f114a0e6593b4fe0071aba39
fsamreen <aziaf2002@yahoo.com>
2017-10-03 13:21:45 +0100

    Update install6.sh

--- install6.sh
+++ install6.sh
@@ -2,9 +2,9 @@
 #run it as source xyz.sh
 echo " Setting environmet variable for netcdf......."
 cd ~/Build_WRF/LIBRARIES
-echo 'export PATH=$DIR/netcdf/bin:$PATH' >>~/.profile
-echo 'export NETCDF=$DIR/netcdf' >>~/.profile
-source ~/.profile
+#echo 'export PATH=$DIR/netcdf/bin:$PATH' >>~/.profile
+#echo 'export NETCDF=$DIR/netcdf' >>~/.profile
+#source ~/.profile
 #export PATH=$DIR/netcdf/bin:$PATH
 #export NETCDF=$DIR/netcdf
 cd ..
