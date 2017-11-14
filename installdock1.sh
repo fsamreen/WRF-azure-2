45a502f06368f60828ad76dec97ecef766618f73
fsamreen <aziaf2002@yahoo.com>
2017-11-14 12:10:32 +0000

    Update installdock1.sh

--- installdock1.sh
+++ installdock1.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-#apt-get install g++ -y
-cd /home/TESTS
-mkdir testfordocker
-wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/Fortran_C_tests.tar
-tar -xf Fortran_C_tests.tar
-#Test #1: Fixed Format Fortran Test: TEST_1_fortran_only_fixed.f
-#gfortran TEST_1_fortran_only_fixed.f
-#./a.out
-
+cd /home/Build_WRF
+mkdir LIBRARIES
+cd LIBRARIES
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/mpich-3.0.4.tar.gz
+wget http://archive.ubuntu.com/ubuntu/pool/universe/m/mpich/mpich_3.2.orig.tar.gz
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/netcdf-4.1.3.tar.gz
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/jasper-1.900.1.tar.gz
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/libpng-1.2.50.tar.gz
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/zlib-1.2.7.tar.gz
+cd /home/Build_WRF/LIBRARIES
+tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
+cd netcdf-4.1.3
+./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
+apt-get install make -y
+make
+make install
