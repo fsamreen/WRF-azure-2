2692b90dd0db535e88cc39c342f3d21cec4a6a2f
fsamreen <aziaf2002@yahoo.com>
2017-11-14 12:44:15 +0000

    Update installdock1.sh

--- installdock1.sh
+++ installdock1.sh
@@ -11,7 +11,7 @@ wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/zlib
 cd /home/Build_WRF/LIBRARIES
 tar xzvf netcdf-4.1.3.tar.gz     #or just .tar if no .gz present
 cd netcdf-4.1.3
-./configure --prefix=$DIR/netcdf --disable-dap --disable-netcdf-4 --disable-shared
+./configure --prefix=/home/Build_WRF/LIBRARIES/netcdf --disable-dap --disable-netcdf-4 --disable-shared
 apt-get install make -y
 make
 make install
