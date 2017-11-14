1cee1e783c4e0662bdf481399ab615cc0bee29ea
fsamreen <aziaf2002@yahoo.com>
2017-11-14 11:21:30 +0000

    Update installdock1.sh

--- installdock1.sh
+++ installdock1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt-get install g++ -y
+#apt-get install g++ -y
 cd /home/TESTS
-wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/Fortran_C_tests.tar
 mkdir testfordocker
+wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/Fortran_C_tests.tar
+tar -xf Fortran_C_tests.tar
+#Test #1: Fixed Format Fortran Test: TEST_1_fortran_only_fixed.f
+#gfortran TEST_1_fortran_only_fixed.f
+#./a.out
+
