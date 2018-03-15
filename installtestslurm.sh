08d47dd21c15eb9cda64798438e0b2252e837470
fsamreen <aziaf2002@yahoo.com>
2018-03-15 13:06:30 +0000

    Update installtestslurm.sh

--- installtestslurm.sh
+++ installtestslurm.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
-# The original script can be downloaded from https://aka.ms/slurmdemo
-# This is modified and includes a lot of bug fixes.
-#
-# About this demo
-#
-#  This demo shows how to run parametric sweep job in a SLURM cluster,
-#  install software, scan a storage container for input, submit a job/task,
-#  and run image conversion tasks on compute nodes.
-#
-# Pre-requisite
-#
-#  1. Deploy a SLURM cluster on Azure using the resource template.
-#  2. Get an Azure storage account, create 2 containers called "input" and "output"
-#  3. Upload some images to the input container.
-#  4. Edit slurmdemo.py to update ACCOUNT_NAME & ACCOUNT_KEY field with your storage account & key
-#  5. Edit this file and replace <admin account> with the account set up from the template
-#  6. Copy slurmdemo.setup.sh (this file), slurmdemo.sh, and slurmdemo.py to the
-#     master node.
-#  7. On the master node, submit the job to start the processing:
-#         "python slurmdemo.py"
-#  8. The task will download images in the input container, conver it to gray scale image and
-#     upload it back to output container. Watch the result from any storage explorer
-#
-#
-
+# 
 # install gcc, gfortran and g++......
 echo "Installing gcc, gfortran and g++......"
 sudo apt-get update
-sudo apt-get install gfortran -y
-sudo apt-get install gcc -y
-sudo apt-get install g++ -y
+sudo apt-get install -y gfortran gcc g++ make m4
+sudo apt-get install nfs-server
+sudo apt-get install openssh-server
+
+sudo mkdir /wrfstorage
+echo "/wrfstorage    *(rw,async)" >> /etc/exports
+
+sudo service nfs-kernel-server restart
+sudo chown wrfuser /wrfstorage
+#sudo apt-get install gfortran -y
+#sudo apt-get install gcc -y
+#sudo apt-get install g++ -y
 
 # Install the python package manager and other dependecies.
 #sudo apt-get install -y python-pip build-essential libssl-dev libffi-dev python-dev
@@ -38,12 +23,11 @@ sudo apt-get install g++ -y
 # install the azure package for Python.
 #pip install azure==0.11.1 azure-storage==0.20.0
 # Get the blob copy utility for Python.
-#wget 'https://raw.githubusercontent.com/Wintellect/blobxfer/master/blobxfer-0.9.9.10.py'
 
 for node in $(sinfo -o "%n" -h|grep -v `hostname`)
 do
 
-  ssh wrfser@$node sudo apt-get -y install gfortran gcc g++
+  ssh wrfser@$node sudo apt-get -y install gfortran gcc g++ make m4 nfs-client openssh-server
   #ssh azureuser@$node sudo apt-get -y install imagemagick
   #ssh azureuser@$node pip install azure==0.11.1 azure-storage==0.20.0
 
