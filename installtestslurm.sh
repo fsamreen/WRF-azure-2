70a33d2e3a91359342b0d079831078c76e250e0b
fsamreen <aziaf2002@yahoo.com>
2018-03-15 11:55:55 +0000

    Update installtestslurm.sh

--- installtestslurm.sh
+++ installtestslurm.sh
@@ -24,24 +24,31 @@
 #
 #
 
+# install gcc, gfortran and g++......
+echo "Installing gcc, gfortran and g++......"
+sudo apt-get update
+sudo apt-get install gfortran -y
+sudo apt-get install gcc -y
+sudo apt-get install g++ -y
+
 # Install the python package manager and other dependecies.
-sudo apt-get install -y python-pip build-essential libssl-dev libffi-dev python-dev
+#sudo apt-get install -y python-pip build-essential libssl-dev libffi-dev python-dev
 # Install image conversion utility.
-sudo apt-get install -y imagemagick
+#sudo apt-get install -y imagemagick
 # install the azure package for Python.
-pip install azure==0.11.1 azure-storage==0.20.0
+#pip install azure==0.11.1 azure-storage==0.20.0
 # Get the blob copy utility for Python.
-wget 'https://raw.githubusercontent.com/Wintellect/blobxfer/master/blobxfer-0.9.9.10.py'
+#wget 'https://raw.githubusercontent.com/Wintellect/blobxfer/master/blobxfer-0.9.9.10.py'
 
 for node in $(sinfo -o "%n" -h|grep -v `hostname`)
 do
 
-  ssh azureuser@$node sudo apt-get -y install python-pip build-essential libssl-dev libffi-dev python-dev
-  ssh azureuser@$node sudo apt-get -y install imagemagick
-  ssh azureuser@$node pip install azure==0.11.1 azure-storage==0.20.0
+  ssh azureuser@$node sudo apt-get -y install gfortran gcc g++
+  #ssh azureuser@$node sudo apt-get -y install imagemagick
+  #ssh azureuser@$node pip install azure==0.11.1 azure-storage==0.20.0
 
-  scp slurmdemo.py azureuser@$node:~
-  scp slurmdemo.sh azureuser@$node:~
+  #scp slurmdemo.py azureuser@$node:~
+  #scp slurmdemo.sh azureuser@$node:~
 
   ssh azureuser@$node wget 'https://raw.githubusercontent.com/Wintellect/blobxfer/master/blobxfer-0.9.9.10.py'
 
